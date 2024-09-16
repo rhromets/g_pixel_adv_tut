85aa573d5d8b76d1bbb0d4154e5086e36af6abaf
Roman Khromets <rhromets@gmail.com>
2024-09-16 22:53:47 +0300

    feat: add first level map

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,30 @@
-import 'package:flame/game.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class PixelAdventure extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:g_pixel_adventure_tutorial/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
+  late final CameraComponent cam;
+
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+
+    return super.onLoad();
+  }
+}
