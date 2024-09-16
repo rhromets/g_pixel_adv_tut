1620fc9e668486c84d6406b79994d340d8e06ad9
Roman Khromets <rhromets@gmail.com>
2024-09-16 18:35:20 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/g_pixel_adventure_tutorial/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.g_pixel_adventure_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:g_pixel_adventure_tutorial/pixel_adventure.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
+  PixelAdventure game = PixelAdventure();
+  runApp(GameWidget(
+    game: kDebugMode ? PixelAdventure() : game,
+  ));
+}
