5728b0b1e08985983c44e63b5c55690fcff17c64
Roman Khromets <rhromets@gmail.com>
2024-09-20 18:15:11 +0300

    feat: add animation of collected fruits

--- lib/components/fruit.dart
+++ lib/components/fruit.dart
@@ -17,6 +17,7 @@ class Fruit extends SpriteAnimationComponent
           size: size,
         );
 
+  bool _collected = false;
   final double stepTime = 0.05;
   final hitbox = CustomHitbox(
     offsetX: 10,
@@ -49,6 +50,21 @@ class Fruit extends SpriteAnimationComponent
   }
 
   void collidedWithPlayer() {
-    removeFromParent();
+    if (!_collected) {
+      animation = SpriteAnimation.fromFrameData(
+        game.images.fromCache('Items/Fruits/Collected.png'),
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: stepTime,
+          textureSize: Vector2.all(32),
+          loop: false,
+        ),
+      );
+      _collected = true;
+    }
+    Future.delayed(
+      const Duration(milliseconds: 400),
+      () => removeFromParent(),
+    );
   }
 }
